3e4ce45b762c0943a760c4ed2d117a9c7aaaee70
xChocoMel <melaniekwetters@hotmail.com>
2016-02-26 15:17:35 +0100

    Bugfix 2

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridReader.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridReader.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GridReader {
-
     private static final String NEW_LINE = System.getProperty("line.separator");
 
     public Cell[][] loadFrom(final String gridContents) {
